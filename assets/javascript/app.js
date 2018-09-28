2db45282e1a69c2da5fbb5d067e4abf5e2a30fc2
Ian Richard <ianrichard03@gmail.com>
2018-09-27 23:37:50 -0500

    linked textboxes to button input for train info.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,7 @@
 $(function(){
     //console.log("bootsrap works.");
     
-
-    //stretch todo collapsable form
-    $('#hideFormButton').click(function(){
-        console.log("buttonworks");
-    });
-
-    //todo declare firebase config object, initialize firebase database
+    //declare firebase object.
     var config = {
         apiKey: "AIzaSyCdYIqcArlVzcxg3ZHkZGwIaCB2N7gBdo8",
         authDomain: "myseconddatabase-5c086.firebaseapp.com",
@@ -17,13 +11,36 @@ $(function(){
         storageBucket: "myseconddatabase-5c086.appspot.com",
         messagingSenderId: "502094714443"
     };
-    
+    //initialize firebase with config object with my firebase settings
     firebase.initializeApp(config);
+
+    //create my database instance and declare variables to store form inputs.
+    var myDatabase = firebase.database();
+    var trainName;
+    var destination;
+    var frequency;
+    var departTime; 
+
+    //stretch todo collapsable form
+    $('#hideFormButton').click(function(){
+        console.log("buttonworks");
+    });
+
+  
+    
 
 
     //todo attach onclick to buttons for form inputs and firebase writes.
     $('#addTrainButton').click(function(){
         console.log("buttonworks");
+            trainName = $('.addTrain').val().trim();
+            destination = $('.addDestination').val().trim();
+            departTime = $('.addDepartTime').val().trim();
+            frequency = $('.addTrainFrequency').val().trim();
+            console.log(trainName);
+            console.log(destination);
+            console.log(departTime);
+            console.log(frequency);
     });
 
     //todo use  .once to display previously entered firebase 
