bae35b04baa144f21145466d8dee2a758c93963e
Ian Richard <ianrichard03@gmail.com>
2018-09-30 16:20:12 -0500

    fixed get Arrival and next train for previously added trains.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -78,7 +78,7 @@ $(function(){
         myDatabase.ref().once("value", function (snapshot) {
             var latestSnapshot = snapshot.val();
             for(var looper in latestSnapshot){
-                nextArrival = getArrival(latestSnapshot[looper].departTime,latestSnapshot[looper].tripTime);
+                getArrival(latestSnapshot[looper].departTime,latestSnapshot[looper].tripTime);
                 $('#trainTable').append(`<tr><td>${latestSnapshot[looper].trainName}</td><td>${latestSnapshot[looper].destination}</td><td>${latestSnapshot[looper].tripTime}</td><td class="nextArrival">${nextArrival}<td class="minutesOut">${minutesOut}</tr>`);
             }
             
@@ -95,31 +95,30 @@ $(function(){
             $('.clock').text(currentTime);
         },1000);
     }
-
+    
+    //get arrival takes in two arguments, start and trip, assigns them to local varibales.
+    //Then I pass the startTime as an argument, along with desired format to moment, and call it's subtract method with argument # to subtract,
+    // and moment object parameter to subtract from???
+    //minutes different get's current time, and then get's difference between it and the start time in minutes.
+    //remainder is assigned the remainder of the difference/train frequency.
+    //minutes out (time till next train) is set to equal the frequency - the remainder.
+    //minutes out and next arrival are returned to the calling function.
     function getArrival(start,trip){
         var frequency = trip;
-        var startTime = start.toString();
-        convertedStartTime = moment(startTime, "HH:mm");
-        var minutesDifferent = moment().diff(moment(convertedStartTime), "minutes");    //I don't understand this.
+        var startTime = start;
+        convertedStartTime = moment(startTime, "HH:mm").subtract(1, "years");
+        var minutesDifferent = moment().diff(moment(convertedStartTime), "minutes");    
         console.log(frequency);
         console.log(startTime);
         console.log(convertedStartTime);
         console.log(minutesDifferent);
-
-        /**
-        if(moment(timeNow).format('HH:mm') <= (moment(start).format('HH:mm'))){
-            minutesOut = start - timeNow;
-            return start + trip;  
-        }else{
-            var x = timeNow - start;
-            var y = x%trip;
-            mintuesOut = y;
-            var n = x/trip;
-            var z = start + (trip*n);
-            return z;
-        }
-    */
-       return "nothing";
+        
+        var remainder = minutesDifferent % frequency;
+        console.log("Remainder is: " + remainder);
+        minutesOut = frequency - remainder;
+        console.log("Minutes until next train:" + minutesOut);
+        var addArrival = moment().add(minutesOut, "minutes");
+        nextArrival = moment(addArrival).format("HH:mm");
     }
     
     
